da9959a59bcb631abd20803ff6edbb77af3933ac
nguyentoi53@gmail.com <nguyentoi53@gmail.com>
2021-11-08 20:13:33 +0700

    add router

---
+++ routes/nhanvien.rounte.js
@@ -0,0 +1,97 @@
+const { Connect } = require('aws-sdk');
+
+const expressLayouts = require('express-ejs-layouts');
+var express = require('express');
+var database = require("../database");
+var router = express.Router();
+router.use(express.json({ extended: false }));
+router.use(express.static('../views'));
+router.use(expressLayouts);
+
+//Nhân Viên
+router.get('/cnkhoa', (req, res) => {
+    //return res.render('CNKhoa');
+    return res.render('./bodyNhanVien/CNKhoa',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Khoa'});
+});
+
+router.get('/cnchuyennganh', (req, res) => {
+    return res.render('./bodyNhanVien/CNChuyenNganh',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Chuyên Ngành'});
+});
+
+router.get('/cnsinhvien', (req, res) => {
+    //const dsnam = null;
+    database.getAllSV(function (results) {
+        //return res.render('CNSinhVien', { listsv: results });
+        return res.render('./bodyNhanVien/CNSinhVien',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Sinh Viên',listsv: results});
+    });
+});
+
+router.get('/cngiangvien', (req, res) => {
+   // return res.render('CNGiangVien');
+    return res.render('./bodyNhanVien/CNGiangVien',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Giảng Viên'});
+});
+
+router.get('/cnmonhp', (req, res) => {
+   // return res.render('CNMonHocPhan');
+    return res.render('./bodyNhanVien/CNMonHocPhan',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Môn Học Phần'});
+});
+
+router.get('/cnlophp', (req, res) => {
+    //return res.render('CNLopHP');
+    return res.render('./bodyNhanVien/CNLopHP',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Lớp Học Phần'});
+});
+
+router.get('/cnnamhoc', (req, res) => {
+    return res.render('./bodyNhanVien/CNNamHoc',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Năm Học'});
+});
+
+router.get('/cnhocky', (req, res) => {
+    return res.render('./bodyNhanVien/CNHocKi',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Học Kì'});
+});
+
+router.get('/cnphonghoc', (req, res) => {
+    return res.render('./bodyNhanVien/CNPhongHoc',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Phòng Học'});
+});
+
+router.get('/chiachuyennganh', (req, res) => {
+    return res.render('./bodyNhanVien/ChiaChuyenNganh',{layout: './layouts/layoutNhanVien' , title: 'Chia Chuyên Ngành'});
+});
+
+router.get('/xeplichhoc', (req, res) => {
+    return res.render('./bodyNhanVien/XepLichHoc',{layout: './layouts/layoutNhanVien' , title: 'Xếp Lịch Học'});
+});
+
+router.get('/xemkhoa', (req, res) => {
+    return res.render('./bodyNhanVien/DMKhoa',{layout: './layouts/layoutNhanVien' , title: 'Xem Khoa'});
+});
+
+router.get('/xemgiangvien', (req, res) => {
+
+    return res.render('./bodyNhanVien/DMGiangVien',{layout: './layouts/layoutNhanVien' , title: 'Xem Giảng Viên'});
+});
+
+router.get('/xemnganh', (req, res) => {
+    
+    return res.render('./bodyNhanVien/DMChuyenNganh',{layout: './layouts/layoutNhanVien' , title: 'Xem Chuyen Ngành'});
+});
+
+router.get('/trangchuNV', (req, res) => {
+    return res.render('./bodyNhanVien/GD_NV_TrangChu',{layout: './layouts/layoutNhanVien' , title: 'Trang Chủ Nhân Viên'});
+});
+
+router.get('/xepkhung', (req, res) => {
+    return res.render('./bodyNhanVien/XepKhung',{layout: './layouts/layoutNhanVien' , title: 'Xếp Khung'});
+});
+
+router.get('/timsv', (req, res) => {
+    return res.render('./bodyNhanVien/TimKiemSV',{layout: './layouts/layoutNhanVien' , title:'Tìm Kiếm Sinh Viên'});
+});
+
+router.get('/timgv', (req, res) => {
+    return res.render('./bodyNhanVien/TimKiemGV',{layout: './layouts/layoutNhanVien' , title: 'Tìm Kiếm Giảng Viên'});
+});
+router.get('/timmonhp', (req, res) => {
+    return res.render('./bodyNhanVien/TimKiemMHP',{layout: './layouts/layoutNhanVien' , title: 'Tìm Kiếm Môn Học Phần'});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,13 +8,11 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 //layouts
 const expressLayouts = require('express-ejs-layouts');
-
 //database NTNT
 var database = require("./database");
 
-
-
-
+var nhanvienRoute = require('./routes/nhanvien.rounte');
+var sinhvienRoute = require('./routes/sinhvien.route');
 
 
 app.use(express.json({ extended: false }));
@@ -31,124 +29,14 @@ app.get('/', (req, res) => {
     return res.render('./bodyChung/TrangChu',{layout: './layouts/layoutChung' , title: 'Trang Chủ'});
 });
 
-
-
 //Nhân Viên
-app.get('/cnkhoa', (req, res) => {
-    //return res.render('CNKhoa');
-    return res.render('./bodyNhanVien/CNKhoa',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Khoa'});
-});
-app.get('/cnchuyennganh', (req, res) => {
-    return res.render('./bodyNhanVien/CNChuyenNganh',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Chuyên Ngành'});
-});
-app.get('/cnsinhvien', (req, res) => {
-    //const dsnam = null;
-    database.getAllSV(function (results) {
-        //return res.render('CNSinhVien', { listsv: results });
-        return res.render('./bodyNhanVien/CNSinhVien',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Sinh Viên',listsv: results});
-    });
-});
-app.get('/cngiangvien', (req, res) => {
-   // return res.render('CNGiangVien');
-    return res.render('./bodyNhanVien/CNGiangVien',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Giảng Viên'});
-});
-app.get('/cnmonhp', (req, res) => {
-   // return res.render('CNMonHocPhan');
-    return res.render('./bodyNhanVien/CNMonHocPhan',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Môn Học Phần'});
-});
+app.use('/nhanvien', nhanvienRoute);
 
-app.get('/cnlophp', (req, res) => {
-    //return res.render('CNLopHP');
-    return res.render('./bodyNhanVien/CNLopHP',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Lớp Học Phần'});
-});
-app.get('/cnnamhoc', (req, res) => {
-    return res.render('./bodyNhanVien/CNNamHoc',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Năm Học'});
-});
-app.get('/cnhocky', (req, res) => {
-    return res.render('./bodyNhanVien/CNHocKi',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Học Kì'});
-});
-app.get('/cnphonghoc', (req, res) => {
-    return res.render('./bodyNhanVien/CNPhongHoc',{layout: './layouts/layoutNhanVien' , title: 'Cập Nhật Phòng Học'});
-});
-app.get('/chiachuyennganh', (req, res) => {
-    return res.render('./bodyNhanVien/ChiaChuyenNganh',{layout: './layouts/layoutNhanVien' , title: 'Chia Chuyên Ngành'});
-});
-app.get('/xeplichhoc', (req, res) => {
-    return res.render('./bodyNhanVien/XepLichHoc',{layout: './layouts/layoutNhanVien' , title: 'Xếp Lịch Học'});
-});
-
-app.get('/xemkhoa', (req, res) => {
-    return res.render('./bodyNhanVien/DMKhoa',{layout: './layouts/layoutNhanVien' , title: 'Xem Khoa'});
-});
-
-
-app.get('/xemgiangvien', (req, res) => {
-
-    return res.render('./bodyNhanVien/DMGiangVien',{layout: './layouts/layoutNhanVien' , title: 'Xem Giảng Viên'});
-});
-
-app.get('/xemnganh', (req, res) => {
-    
-    return res.render('./bodyNhanVien/DMChuyenNganh',{layout: './layouts/layoutNhanVien' , title: 'Xem Chuyen Ngành'});
-});
-
-app.get('/trangchuNV', (req, res) => {
-    return res.render('./bodyNhanVien/GD_NV_TrangChu',{layout: './layouts/layoutNhanVien' , title: 'Trang Chủ Nhân Viên'});
-});
-app.get('/xepkhung', (req, res) => {
-    return res.render('./bodyNhanVien/XepKhung',{layout: './layouts/layoutNhanVien' , title: 'Xếp Khung'});
-});
-app.get('/timsv', (req, res) => {
-    return res.render('./bodyNhanVien/TimKiemSV',{layout: './layouts/layoutNhanVien' , title:'Tìm Kiếm Sinh Viên'});
-});
-
-app.get('/timgv', (req, res) => {
-    return res.render('./bodyNhanVien/TimKiemGV',{layout: './layouts/layoutNhanVien' , title: 'Tìm Kiếm Giảng Viên'});
-});
-app.get('/timmonhp', (req, res) => {
-    return res.render('./bodyNhanVien/TimKiemMHP',{layout: './layouts/layoutNhanVien' , title: 'Tìm Kiếm Môn Học Phần'});
-});
 //Sinh Viên
-app.get('/trangchu', (req, res) => {
-
-    return res.render('./bodySinhVien/GD_SV_trangchu',{layout: './layouts/layoutSinhVien' , title: 'Trang Chủ Sinh Viên'});
-});
-
-app.get('/dangkyhp', (req, res) => {
-    
-    return res.render('./bodySinhVien/GD_SV_dkhp',{layout: './layouts/layoutSinhVien' , title: 'Đăng Ký Học Phần'});
-});
+app.use('/sinhvien', sinhvienRoute);
 
 
-app.get('/xemttcn', (req, res) => {
-  
-    return res.render('./bodySinhVien/GD_SV_xemttcn',{layout: './layouts/layoutSinhVien' , title: 'Xem Thông Tin Cá Nhân'});
-});
-
-
-app.get('/doimk', (req, res) => {
- 
-    return res.render('./bodySinhVien/GD_SV_doimk',{layout: './layouts/layoutSinhVien' , title: 'Đổi Mật Khẩu'});
-});
-
-app.get('/xemcongno', (req, res) => {
-
-    return res.render('./bodySinhVien/GD_SV_xemcongno',{layout: './layouts/layoutSinhVien' , title: 'Xem Công Nợ'});
-});
-
-app.get('/xemctk', (req, res) => {
-
-    return res.render('./bodySinhVien/GD_SV_xemctkhung',{layout: './layouts/layoutSinhVien' , title: 'Xem Chương Trình Khung'});
-});
-
-
-app.get('/xemlh', (req, res) => {
-
-    return res.render('./bodySinhVien/GD_SV_xemlh',{layout: './layouts/layoutSinhVien' , title: 'Xem Lịch Học'});
-});
- 
 // không menu
-
 app.get('/cnsinhvien/add-sv', (req, res) => {
     //res.render('GD_NV_From_Add_SV');
     return res.render('./bodyKhongMenu/GD_NV_From_Add_SV',{layout: './layouts/layoutKhongMenu' , title: 'Thêm Sinh Viên'});
@@ -206,13 +94,10 @@ app.post('/update_sv', upload.fields([]), (req, res) => {
     //console.log(req.body);
 });
 
-
 app.post('/dangnhaptong', upload.fields([]), (req, res) => {
     var username = req.body.tendn;
     var pass = req.body.matkhau;
     let encryptedPass = '';
-
-
     bcrypt.genSalt(saltRounds, (err, salt) => {
         bcrypt.hash(pass, salt, function (err, hash) {
             // Store hash in your password DB.
