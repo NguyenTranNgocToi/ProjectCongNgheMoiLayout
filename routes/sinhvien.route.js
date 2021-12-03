ecccf813804225defde703c49a4a38894290f99b
nguyentoi53@gmail.com <nguyentoi53@gmail.com>
2021-12-03 13:32:43 +0700

    xong

--- routes/sinhvien.route.js
+++ routes/sinhvien.route.js
@@ -4,6 +4,13 @@ const expressLayouts = require('express-ejs-layouts');
 var express = require('express');
 var database = require("../database");
 var controllerSV = require("../controller/sinhvien.controller");
+const controllerTDMK = require('../controller/TDMK_Ctr.controller');
+const controllerTTCN = require('../controller/TTCN_Ctr.contronller');
+const controllerCTK = require('../controller/CTK_Ctr.controller');
+const controllerCN = require('../controller/DS_CongNo.controller');
+const controllerXLH = require('../controller/XLH_Ctr.controller');
+const controllerXKQHT = require('../controller/KQHT_Ctr.controller');
+const controllerDKHP = require('../controller/DKHP_Ctr.controller');
 var router = express.Router();
 router.use(express.json({ extended: false }));
 router.use(express.static('../views'));
@@ -18,24 +25,24 @@ router.get('/trangchu', (req, res) => {
     return res.render('./bodySinhVien/GD_SV_trangchu',{layout: './layouts/layoutSinhVien' , title: 'Trang Chủ Sinh Viên'});
 });
 
-router.get('/dangkyhp', controllerSV.dangkyhocphan);
+router.get('/dangkyhp', controllerDKHP.dangkyhocphan);
 
 //xem thông tin cá nhân ntnt
-router.get('/xemttcn', controllerSV.xemthongtincanha);
+router.get('/xemttcn', controllerTTCN.xemthongtincanhan);
 
 //đổi mật khẩu get ntnt
-router.get('/doimk', controllerSV.doiMatKhauSV);
+router.get('/doimk', controllerTDMK.doiMatKhauSV);
 
 //đổi mật khẩu post ntnt
-router.post('/doimatkhau',upload.fields([]), controllerSV.postDoiMatKhauSV);
+router.post('/doimatkhau',upload.fields([]), controllerTDMK.postDoiMatKhauSV);
 
-router.get('/xemcongno',controllerSV.xemcongno)
+router.get('/xemcongno',controllerCN.xemcongno);
 
 //xem chương trình khung ntnt
-router.get('/xemctk',controllerSV.xemchuongtrinhkhung);
+router.get('/xemctk',controllerCTK.xemchuongtrinhkhung);
 
 
-router.get('/xemlh', controllerSV.xemlichhoc);
+router.get('/xemlh', controllerXLH.xemlichhoc);
 
 router.get('/dangxuat', (req, res) => {
     res.clearCookie('mssv');
@@ -43,7 +50,7 @@ router.get('/dangxuat', (req, res) => {
     return res.redirect('/');
 });
 
-router.get('/ketquahoctap', controllerSV.xemketquahoctap);
+router.get('/ketquahoctap', controllerXKQHT.xemketquahoctap);
 
 
 module.exports = router;
